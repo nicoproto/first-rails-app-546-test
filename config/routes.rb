afd9bf86c9deb969c89578881ccdbb935923d63f
Nico Proto <nicolasproto@gmail.com>
2021-02-28 17:54:11 +0100

    Added pages controller with contact page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get '/about', to: 'pages#about'
+
+  # Generic syntax
+  # verb 'path', to: 'controller_name#action_name'
 end
